bef2697c42593408fe2c75e63151fb5f06121619
Brenda <bee@bee.com>
2022-03-14 00:24:07 +0300

    This commit adds all files to the repo

--- js/scripts.js
+++ js/scripts.js
@@ -35,61 +35,60 @@ $(document).ready(function () {
             $('#product-content').hide('fast');
         });
     });
+
+    //portfolio
+    $('#work1').mouseenter(function () {
+        $('.work-heading').filter('#work1-title').show("slow");
+    }).mouseleave(function () {
+        $('#work1-title').hide("slow");
+    });
+
+
+    $('#work2').mouseenter(function () {
+        $('.work-heading').filter('#work2-title').show("slow");
+    }).mouseleave(function () {
+        $('#work2-title').hide("slow");
+    });
+
+    $('#work3').mouseenter(function () {
+        $('.work-heading').filter('#work3-title').show("slow");
+    }).mouseleave(function () {
+        $('#work3-title').hide("slow");
+    });
+
+
+    $('#work4').mouseenter(function () {
+        $('.work-heading').filter('#work4-title').show("slow");
+    }).mouseleave(function () {
+        $('#work4-title').hide("slow");
+    });
+
+    $('#work5').mouseenter(function () {
+        $('.work-heading').filter('#work5-title').show("slow");
+    }).mouseleave(function () {
+        $('#work5-title').hide("slow");
+    });
+
+
+    $('#work6').mouseenter(function () {
+        $('.work-heading').filter('#work6-title').show("slow");
+    }).mouseleave(function () {
+        $('#work6-title').hide("slow");
+    });
+
+    $('#work7').mouseenter(function () {
+        $('.work-heading').filter('#work7-title').show("slow");
+    }).mouseleave(function () {
+        $('#work7-title').hide("slow");
+    });
+    $('#work8').mouseenter(function () {
+        $('.work-heading').filter('#work8-title').show("slow");
+    }).mouseleave(function () {
+        $('#work8-title').hide("slow");
+    });
 });
 
-$(document).ready(function(){
-    $("#work1").mouseover(function(){
-      $("#overlay").show();
-    }).mouseout(function(){
-      $("#overlay").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work2").mouseover(function(){
-      $("#overlay2").show();
-    }).mouseout(function(){
-      $("#overlay2").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work3").mouseover(function(){
-      $("#overlay3").show();
-    }).mouseout(function(){
-      $("#overlay3").hide();
-    });
-  });
-  $(document).ready(function(){
-    $("#work4").mouseover(function(){
-      $("#overlay4").show();
-    }).mouseout(function(){
-      $("#overlay4").hide();
-    });
-  });
-  
-  $(document).ready(function(){
-    $("#work5").mouseover(function(){
-      $("#overlay5").show();
-    }).mouseout(function(){
-      $("#overlay5").hide();
-    });
-    $("#work6").mouseover(function(){
-      $("#overlay6").show();
-    }).mouseout(function(){
-      $("#overlay6").hide();
-    });
-    $("#work7").mouseover(function(){
-      $("#overlay7").show();
-    }).mouseout(function(){
-      $("#overlay7").hide();
-    });
-    $("#work8").mouseover(function(){
-      $("#overlay8").show();
-    }).mouseout(function(){
-      $("#overlay8").hide();
-    });
-  });
-
-  //user Interface logic
+//user Interface logic
 $(document).ready(function () {
     $('form.contact').on('submit', function (e) {
         e.preventDefault();
